da1f9ca298e64086fa0b9caf59c32b5b9681a696
Tibor Kalman <tibor.kalman@gwdg.de>
2017-07-20 13:57:30 +0200

    Throwing an error when the servlet's configuration file was not found in {HANDLE_SVR}/handlereverselookupservlet.properties

--- src/de/dkrz/handlereverselookupservlet/HRLSApplication.java
+++ src/de/dkrz/handlereverselookupservlet/HRLSApplication.java
@@ -33,7 +33,7 @@ public class HRLSApplication extends Application {
 		// Search for simple config file in $HANDLE_SVR/handlereverselookupservlet.properties
 		String handleHome = System.getenv("HANDLE_SVR");
 		if (handleHome == null) {
-			throw new InvalidConfigException("This servlet requires the HANDLE_SVR environmental variable." +
+			throw new InvalidConfigException("This servlet requires the HANDLE_SVR environmental variable. " +
 				"HANDLE_SRV should point to the Handle server instance's home directory, " +
 				"where its configuration files are located (e.g. '/hs/svr_1').");
 		} else {
@@ -42,6 +42,8 @@ public class HRLSApplication extends Application {
 				Properties configFileProps = new Properties();
 				configFileProps.load(new FileInputStream(configFile));
 				env.putAll(configFileProps);
+			} else {
+				throw new InvalidConfigException("The servlet's configuration file " + configFile + " cannot be found!");
 			}
 		}
 		ReverseLookupConfig hrlsConfig = new ReverseLookupConfig(servletContext, env);
